85489876dd29cbde1bcef878dad40033358e4433
DhilipThangaraj <DhilipThangaraj777@gmail.com>
2021-05-28 10:30:16 +0530

    Added cookies and LS facility when sign in happens - This helper.js user for token and user details saving

---
+++ client/src/auth/helpers.js
@@ -0,0 +1,71 @@
+/**
+ * @summary - The files especially used for saving the token and user details when the signin happens.
+ */
+
+import cookie from "js-cookie";
+
+//set in cookie
+export const setCookie = (key, value) => {
+  if (window !== "undefined") {
+    cookie.set(key, value, {
+      expires: 1,
+    });
+  }
+};
+
+//remove from cookie
+export const removeCookie = (key) => {
+  if (window !== "undefined") {
+    cookie.remove(key, {
+      expires: 1,
+    });
+  }
+};
+
+//get from cookie such as stored token and will be useful when we need to make request to server with token.
+
+export const getCookie = (key) => {
+  if (window !== "undefined") {
+    return cookie.get(key);
+  }
+  return "";
+};
+
+//set in localstorage
+export const setLocalStorage = (key, value) => {
+  if (window !== "undefined") {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+};
+
+//remove from localstorage
+export const removeLocalStorage = (key) => {
+  if (window !== "undefined") {
+    localStorage.removeItem(key);
+  }
+};
+
+//access user details from localstorage - isAuth equals to getLocalStorageUserDetails.Basically checking the user
+//as authentic user or not
+
+export const isAuth = (key) => {
+  if (window !== "undefined") {
+    const cookieChecked = getCookie("token");
+    if (cookieChecked) {
+      if (localStorage.getItem("user")) {
+        return JSON.parse(localStorage.getItem("user"));
+      }
+    } else {
+      return false;
+    }
+  }
+};
+
+//authendicate user by passing data to cookie and localstorage during signin
+
+export const authendicate = (response, next) => {
+  console.log("AUTHENTICATE HEPLER ON SIGNIN RESPONSE");
+  setCookie("token", response.data.token);
+  setLocalStorage("user", response.data.user);
+  next();
+};
