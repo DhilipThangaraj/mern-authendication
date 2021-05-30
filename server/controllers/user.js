89fa20b7589a384b4f725536d3587fb972ad9c8f
DhilipThangaraj <DhilipThangaraj777@gmail.com>
2021-05-30 06:03:52 +0530

    Profile update route made and necessary functionality written

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -13,3 +13,43 @@ exports.read = (req, res) => {
     return res.json(user);
   });
 };
+
+exports.update = (req, res) => {
+  const { name, password } = req.body;
+  User.findById({ _id: req.user._id }).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User not found",
+      });
+    }
+    if (!name) {
+      return res.status(400).json({
+        error: "Name is required",
+      });
+    } else {
+      user.name = name;
+    }
+
+    if (password) {
+      if (password.length < 6) {
+        return res.status(400).json({
+          error: "Password should be min 6 characters long",
+        });
+      } else {
+        user.password = password;
+      }
+    }
+
+    user.save((err, updatedUser) => {
+      if (err) {
+        console.log("USER UPDATE ERROR", err);
+        return res.status(400).json({
+          error: "User update failed",
+        });
+      }
+      updatedUser.hashed_password = undefined;
+      updatedUser.salt = undefined;
+      return res.json(updatedUser);
+    });
+  });
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 //import controller
 const { read } = require("../controllers/user");
+const { update } = require("../controllers/user");
 
 //Import api protecting function
 const { requireSignin } = require("../controllers/auth");
 
 router.get("/user/:id", requireSignin, read);
+router.put("/user/update", requireSignin, update);
 
 module.exports = router;
