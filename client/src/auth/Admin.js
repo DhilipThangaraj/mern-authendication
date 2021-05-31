f5aa8c86b17f69fee5622f91eb74493bfee0b7bd
DhilipThangaraj <DhilipThangaraj777@gmail.com>
2021-05-31 09:14:52 +0530

    Updated admin page, this page almost similar to the private page

--- client/src/auth/Admin.js
+++ client/src/auth/Admin.js
@@ -1,10 +1,138 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../core/Layout";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { isAuth, getCookie, signout, updateUser } from "./helpers";
 
-const Admin = () => {
+const Admin = ({ history }) => {
+  const [values, setValues] = useState({
+    role: "",
+    name: "",
+    email: "",
+    password: "",
+    buttonText: "Submit",
+  });
+
+  const token = getCookie("token");
+
+  const { role, name, email, password, buttonText } = values;
+
+  useEffect(() => {
+    loadProfile();
+  }, []);
+
+  const loadProfile = () => {
+    axios({
+      method: "GET",
+      url: `${process.env.REACT_APP_API}/user/${isAuth()._id}`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        console.log("PROFILE UPDATE", response);
+        const { role, name, email } = response.data;
+        setValues({ ...values, role, name, email });
+      })
+      .catch((error) => {
+        console.log("PRIVATE PROFILE UPDATE ERROR");
+        if (error.response.status === "401") {
+          signout(() => {
+            history.push("/");
+          });
+        }
+      });
+  };
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, buttonText: "Submitting" });
+    axios({
+      method: "PUT",
+      url: `${process.env.REACT_APP_API}/admin/update`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: { name, password },
+    })
+      .then((response) => {
+        console.log("PRIVATE PROFILE UPDATE SUCCESS ", response);
+        updateUser(response, () => {
+          setValues({
+            ...values,
+            buttonText: "Submitted",
+          });
+          toast.success("Profile updated successfully");
+          setTimeout(() => {
+            setValues({
+              ...values,
+              buttonText: "Submit",
+            });
+          }, 5000);
+        });
+      })
+      .catch((error) => {
+        console.log("PRIVATE PROFILE UPDATE ERROR", error.response.data.error);
+        setValues({ ...values, buttonText: "Submit" });
+        toast.error(error.response.data.error);
+      });
+  };
+
+  const updateForm = () => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Role</label>
+        <input
+          defaultValue={role}
+          type="text"
+          className="form-control"
+          disabled
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          onChange={handleChange("name")}
+          value={name}
+          type="text"
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input value={email} type="email" className="form-control" disabled />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          onChange={handleChange("password")}
+          value={password}
+          type="password"
+          className="form-control"
+        />
+      </div>
+      <br />
+      <div>
+        <button className="btn btn-primary" onClick={handleSubmit}>
+          {buttonText}
+        </button>
+      </div>
+    </form>
+  );
+
   return (
     <Layout>
-      <h1>Admin Page</h1>
+      <div className="col-d-6 offset-md-3">
+        <ToastContainer />
+        <h1 className="pt-5 text-center">Admin</h1>
+        <p className="lead text-center">Profile update</p>
+        {updateForm()}
+      </div>
     </Layout>
   );
 };
